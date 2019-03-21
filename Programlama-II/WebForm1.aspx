e9c834f4795fa2fbfc464992a966a4d9bd586f22
Yasin Can <yasincan.cs@outlook.com>
2019-03-21 23:42:24 +0300

    OK.

---
+++ Programlama-II/WebForm1.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Programlama_II.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 118px;
+        }
+        .auto-style2 {
+            width: 118px;
+            height: 26px;
+        }
+        .auto-style3 {
+            height: 26px;
+        }
+    </style>
+</head>
+<body style="width: 459px">
+    <form id="form1" runat="server">
+        <div>
+            <table style="width: 322px; height: 122px"
+                >
+                <tr>
+                    <td class="auto-style2">Ülke Adı :</td>
+                    <td class="auto-style3">
+                        <asp:DropDownList ID="ulkeDropDownList" runat="server" Height="16px" Width="185px" AutoPostBack="True" OnSelectedIndexChanged="ulkeDropDownList_SelectedIndexChanged" DataTextField="--Lütfen ülke seçin--">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">İl Adı :</td>
+                    <td>
+                        <asp:DropDownList ID="ilDropDownList" runat="server" Height="16px" Width="185px" AutoPostBack="True" OnSelectedIndexChanged="ilDropDownList_SelectedIndexChanged">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">İlçe Adı :</td>
+                    <td>
+                        <asp:DropDownList ID="ilceDropDownList" runat="server" Height="16px" Width="185px">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                   
+                    <td colspan="2" style="margin-left: 40px">
+                        <asp:Button ID="Button1" runat="server" Height="41px" Text="Seçimi Göster" Width="304px" OnClick="Button1_Click" />
+                    </td>
+                </tr>
+            </table>
+            <asp:Label ID="ulkeLabel" runat="server"></asp:Label>
+            <br />
+            <asp:Label ID="ilLabel" runat="server"></asp:Label>
+            <br />
+            <asp:Label ID="ilceLabel" runat="server"></asp:Label>         
+        </div>
+    </form>
+</body>
+</html>
---
+++ Programlama-II/WebForm1.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Programlama_II
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyCountry"].ConnectionString);
+        SqlDataReader reader;
+        SqlCommand command;
+
+        private void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ilDropDownList.Enabled = false;
+                ilceDropDownList.Enabled = false;
+                command = new SqlCommand("Select * From CountryMaster", MyConnection);
+                MyConnection.Open();
+                reader = command.ExecuteReader();
+                ulkeDropDownList.DataSource = reader;
+                ulkeDropDownList.DataValueField = "ID";
+                ulkeDropDownList.DataTextField = "Name";
+                ulkeDropDownList.DataBind();
+                reader.Close();
+                MyConnection.Close();
+            }          
+        } 
+        protected void ulkeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {    
+            if (ulkeDropDownList.AutoPostBack)
+            {
+                ilDropDownList.Enabled = true;
+                command = new SqlCommand("Select * From StateMaster Where CountryID=' " + ulkeDropDownList.SelectedValue + " ' ", MyConnection);
+                MyConnection.Open();
+                reader = command.ExecuteReader();
+                ilDropDownList.DataSource = reader;
+                ilDropDownList.DataValueField = "ID";
+                ilDropDownList.DataTextField = "Name";
+                ilDropDownList.DataBind();
+                reader.Close();
+                MyConnection.Close();
+            }
+        }
+        protected void ilDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ilDropDownList.AutoPostBack)
+            {
+                ilceDropDownList.Enabled = true;
+                command = new SqlCommand("Select * From CityMaster Where StateID=' " + ilDropDownList.SelectedValue + " ' ", MyConnection);
+                MyConnection.Open();
+                reader = command.ExecuteReader();
+                ilceDropDownList.DataSource = reader;
+                ilceDropDownList.DataValueField = "ID";
+                ilceDropDownList.DataTextField = "Name";
+                ilceDropDownList.DataBind();
+                reader.Close();
+                MyConnection.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ulkeLabel.Text = "Ülke Adı:" + ulkeDropDownList.SelectedItem.Text;
+            ilLabel.Text = "İl Adı: " + ilDropDownList.SelectedItem.Text;
+            ilceLabel.Text = "İlçe Adı: " + ilceDropDownList.SelectedItem.Text + " olarak seçilmiştir.";   
+        }
+    }
+}
